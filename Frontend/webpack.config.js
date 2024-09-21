552f02d2efd84f772b4eb40165eb6f97d18e632c
6kumaran <88043642+6kumaran@users.noreply.github.com>
2024-09-21 22:29:26 +0530

    Create webpack.config.js

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = {
+  // other configurations...
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      // Add other Node.js core modules here if needed
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
